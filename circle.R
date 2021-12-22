dcba03536bcc20d5fcc6e4eafbd3435214f8bf16
savaged <savagedavid@gmail.com>
2021-12-22 09:34:01 +0000

    credit to writer on stack exchange

--- circle.R
+++ circle.R
@@ -1,12 +1,9 @@
-# initialize a plot
 plot(c(-1, 1), c(-1, 1), type = "n")
 
-# prepare "circle data"
-radius = 1
-center_x = 0
-center_y = 0
-theta = seq(0, 2 * pi, length = 200) # angles for drawing points around the circle
+r = 1
+x = 0
+y = 0
+t = seq(0, 2 * pi, length = 200)
 
-# draw the circle
-lines(x = radius * cos(theta) + center_x, y = radius * sin(theta) + center_y)
+lines(x = r * cos(t) + x, y = r * sin(t) + y)
 
